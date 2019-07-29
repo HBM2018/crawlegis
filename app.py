abbf57ba1146d67af9a6280f180423ba06fec1a8
HBM2018 <34553183+HBM2018@users.noreply.github.com>
2019-07-29 13:57:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-    
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/deploy", methods=['GET'])    
+def deploy():
+    return "<h1>Testando deploy GitHub x Heroku </h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
