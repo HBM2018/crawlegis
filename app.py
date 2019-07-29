8456b1bb1742dd300f5a9eb4765992820423ece4
HBM2018 <34553183+HBM2018@users.noreply.github.com>
2019-07-29 10:36:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def deploy():
 
 def main():
 #    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+#    app.run(host="0.0.0.0", port=port)
     app.run(host="0.0.0.0")
 
 if __name__ == "__main__":
