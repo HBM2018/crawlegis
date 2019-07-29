50f060e05251d655a24b568299a45c5aa92dd26e
HBM2018 <34553183+HBM2018@users.noreply.github.com>
2019-07-29 13:14:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def deploy():
     return "<h1>Testando deploy GitHub x Heroku </h1>"
 
 def main():
-#   port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 #   app.run(host="0.0.0.0")
 
